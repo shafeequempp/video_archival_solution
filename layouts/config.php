e8291e2148dc4e5a49e7a8a3dea8218cb877f77d
Shafeeque <shafeeque@mpp.co.in>
2025-10-07 10:36:05 +0530

    config

--- layouts/config.php
+++ layouts/config.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-define("BASE_URL", "http://localhost/video_archival_solution/");
+define("BASE_URL", "https://videos.mpp.in/");
 
 session_start();
 if (!isset($_SESSION['user_id'])) {
