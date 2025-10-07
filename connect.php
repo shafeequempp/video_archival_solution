1870c672cf8ac509e04c4bf6622501929db0ad2a
Shafeeque <shafeeque@mpp.co.in>
2025-10-07 10:32:56 +0530

    connection

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root"; 
-$password = ''; 
-$dbname = "video_archival_solution";
+$servername = "10.2.153.58";
+$username = "user_videoarchival"; 
+$password = 'Fgt$%7896JlljP(*7rchj#'; 
+$dbname = "mpp_videoarchival";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
